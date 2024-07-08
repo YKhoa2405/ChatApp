52599dcf1f904a38d64a11f86d70a21469e1a2c4
YKhoa2405 <nykhoa2405@gmail.com>
2024-07-08 17:05:40 +0700

    Wellcome, Login, SignUp screen

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.chatapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        setupButtonClicks();
+    }
+
+    private void setupButtonClicks() {
+        TextView loginLink = findViewById(R.id.loginLink);
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToActivity(LoginActivity.class);
+            }
         });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToActivity(SignupActivity.class);
+            }
+        });
+
+        // Các nút khác bạn có thể setup tại đây
+    }
+
+    private void navigateToActivity(Class<?> activityClass) {
+        Intent intent = new Intent(MainActivity.this, activityClass);
+        startActivity(intent);
     }
 }
